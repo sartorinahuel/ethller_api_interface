48cf2aa4bb573e7f8cce354d413e72118b000c8e
Nahuel Sartori <sartornahuel@gmail.com>
2021-01-29 16:11:37 -0300

    Update pool_stats.dart

--- lib/src/domain/classes/pool/pool_stats.dart
+++ lib/src/domain/classes/pool/pool_stats.dart
@@ -1,33 +1,33 @@
 part of ethller_api_interface;
 
 class PoolStats {
-	double hashRate;
-	int miners;
-	int workers;
-	double blocksPerHour;
+  num hashRate;
+  int miners;
+  int workers;
+  num blocksPerHour;
 
-	PoolStats({
-		this.hashRate,
-		this.miners,
-		this.workers,
-		this.blocksPerHour,
-	});
+  PoolStats({
+    this.hashRate,
+    this.miners,
+    this.workers,
+    this.blocksPerHour,
+  });
 
-	factory PoolStats.fromJson(Map<String, dynamic> json) {
-		return PoolStats(
-			hashRate: json['hashRate'] as num,
-			miners: json['miners'] as int,
-			workers: json['workers'] as int,
-			blocksPerHour: json['blocksPerHour'] as num,
-		);
-	}
+  factory PoolStats.fromJson(Map<String, dynamic> json) {
+    return PoolStats(
+      hashRate: (json['hashRate'] as num) / 1000000000000, //For TH/s
+      miners: json['miners'] as int,
+      workers: json['workers'] as int,
+      blocksPerHour: json['blocksPerHour'] as num,
+    );
+  }
 
-	Map<String, dynamic> toJson() {
-		return {
-			'hashRate': hashRate,
-			'miners': miners,
-			'workers': workers,
-			'blocksPerHour': blocksPerHour,
-		};
-	}
+  Map<String, dynamic> toJson() {
+    return {
+      'hashRate': hashRate,
+      'miners': miners,
+      'workers': workers,
+      'blocksPerHour': blocksPerHour,
+    };
+  }
 }
