4c71f6983b6301f1d8a25add2af2a44085e246c9
Nahuel Sartori <sartornahuel@gmail.com>
2021-01-30 01:22:50 -0300

    Update globals.dart

--- lib/src/domain/globals.dart
+++ lib/src/domain/globals.dart
@@ -4,6 +4,7 @@ part of ethller_api_interface;
 int coinsHistoriesRefreshRate = 2; //in minutes
 int coinsDataRefreshRate = 48; //in minutes
 int poolDataRefreshRate = 5; //in minutes
+int minersDataRefreshRate = 2; //in minutes
 String walletId = 'walletId';
 
 //Dependencies
