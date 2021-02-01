36871df1cfabe3ccfe1cee8b9777479f522a11f4
Nahuel <nsartori@openpass.com.ar>
2021-01-31 22:22:28 -0300

    fixes

--- lib/src/domain/classes/miners/miner_payout.dart
+++ lib/src/domain/classes/miners/miner_payout.dart
@@ -18,7 +18,7 @@ class MinerPayout {
   MinerPayout.fromJson(Map<String, dynamic> json) {
     start = json['start'];
     end = json['end'];
-    amount = (json['amount'] as String).toDoubleFromBigInt();
+    amount = json['amount'] / 1000000000000000000;
     txHash = json['txHash'];
     paidOn = DateTime.fromMillisecondsSinceEpoch(json['paidOn'] * 1000);
   }
